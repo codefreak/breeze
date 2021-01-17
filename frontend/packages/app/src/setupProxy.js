3e9aacb34218ed257c93e4efa920a3706f20a45d
Henning Kasch <henning.kasch@gmail.com>
2021-01-17 15:05:24 +0100

    Fix dev proxy setup

---
+++ frontend/packages/app/src/setupProxy.js
@@ -0,0 +1,16 @@
+// workaround for https://github.com/facebook/create-react-app/issues/5280
+
+const proxy = require('http-proxy-middleware')
+
+const proxyHost = process.env.NODE_PROXY_HOST || 'localhost'
+const proxyPort = process.env.NODE_PROXY_PORT || '3000'
+const proxyUrl = `${proxyHost}:${proxyPort}`
+
+module.exports = function (app) {
+  app.use(
+    proxy('/graphql', {
+      target: `ws://${proxyUrl}`,
+      ws: true
+    })
+  )
+}
